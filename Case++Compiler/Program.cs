1a44ec460fc5ed5b295470a4e2f51be737fa9455
gorkthe134th <gork137k@gmail.com>
2026-02-11 12:04:31 +0200

    Proof of Concept

--- Case++Compiler/Program.cs
+++ Case++Compiler/Program.cs
@@ -1,7 +1,30 @@
-﻿internal class Program
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+internal partial class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //StreamReader reader = new("test.txt");
+        Regex regex = Number();
+        List<char> list = new(40);
+
+        list.AddRange("1536asdf");
+        var span = CollectionsMarshal.AsSpan(list);
+        var e = regex.EnumerateMatches(span);
+        Console.WriteLine(e.MoveNext());
+        Console.WriteLine(span.Slice(e.Current.Index, e.Current.Length));
+
+        list.Insert(0, '-');
+        span = CollectionsMarshal.AsSpan(list);
+        e = regex.EnumerateMatches(span);
+        Console.WriteLine(e.MoveNext());
+        Console.WriteLine(span.Slice(e.Current.Index, e.Current.Length));
+
+        list.Insert(0, 'a');
+        Console.WriteLine(regex.IsMatch(CollectionsMarshal.AsSpan(list)));
     }
+
+    [GeneratedRegex(@"^[+-]?[0-9]+")]
+    private static partial Regex Number();
 }
