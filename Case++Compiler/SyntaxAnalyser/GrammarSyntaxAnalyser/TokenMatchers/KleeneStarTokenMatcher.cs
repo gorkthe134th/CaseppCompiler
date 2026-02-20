7764a0185e1d0811d4f39d9aecf91dfa2d9c623f
gorkthe134th <gork137k@gmail.com>
2026-02-20 03:25:27 +0200

    Fix Kleene Star

--- Case++Compiler/SyntaxAnalyser/GrammarSyntaxAnalyser/TokenMatchers/KleeneStarTokenMatcher.cs
+++ Case++Compiler/SyntaxAnalyser/GrammarSyntaxAnalyser/TokenMatchers/KleeneStarTokenMatcher.cs
@@ -1,18 +1,14 @@
 ﻿using CaseppCompiler.LexicalAnalyser.Tokens;
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace CaseppCompiler.SyntaxAnalyser.GrammarSyntaxAnalyser.TokenMatchers
 {
     internal class KleeneStarTokenMatcher(string name, TokenMatcher matcher) : TokenMatcher(name)
     {
         public override bool? TryMatch(IEnumerator<Token> tokens)
         {
-            bool? match = null;
-            while ((match |= matcher.TryMatch(tokens)) == true) ;
-            return match;
+            bool? match, matchSoFar = null;
+            do matchSoFar |= match = matcher.TryMatch(tokens); while (match == true);
+            return matchSoFar;
         }
     }
 }
